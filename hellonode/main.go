23ff2d13a9d0ab8f0222ed3533bbf33a81087cdd
ChanderG <chanderg@chandergovind.org>
2017-06-09 09:59:59 +0530

    integ service discovery and client management
    every node has to create a dedicated client for every other node that it
    discovers via the service discovery mechanism

--- hellonode/main.go
+++ hellonode/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	hs "github.com/chanderg/grpc-p2p/helloservice"
@@ -29,7 +30,7 @@ type node struct {
 
 // SayHello implements helloworld.GreeterServer
 func (n *node) SayHello(ctx context.Context, in *hs.HelloRequest) (*hs.HelloReply, error) {
-	return &hs.HelloReply{Message: "Hello from" + n.Name}, nil
+	return &hs.HelloReply{Message: "Hello from " + n.Name}, nil
 }
 
 // Start listening/service.
@@ -53,6 +54,7 @@ func (n *node) StartListening() {
 }
 
 // Register self with the service discovery module.
+// This implementation simply uses the key-value store. One major drawback is that when nodes crash. nothing is updated on the key-value store. Services are a better fit and should be used eventually.
 func (n *node) registerService() {
 	config := api.DefaultConfig()
 	config.Address = n.SDAddress
@@ -77,6 +79,8 @@ func (n *node) registerService() {
 // Start the node.
 // This starts listening at the configured address. It also sets up clients for it's peers.
 func (n *node) Start() {
+	// init required variables
+	n.Clients = make(map[string]hs.HelloServiceClient)
 
 	// start service / listening
 	go n.StartListening()
@@ -89,7 +93,50 @@ func (n *node) Start() {
 
 	// wait for other nodes to come up
 	for {
-		time.Sleep(1 * time.Minute)
+		time.Sleep(20 * time.Second)
+		n.GreetAll()
+	}
+}
+
+// Setup a new grpc client for contacting the server at addr.
+func (n *node) SetupClient(name string, addr string) {
+
+	// setup connection with other node
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	n.Clients[name] = hs.NewHelloServiceClient(conn)
+
+	r, err := n.Clients[name].SayHello(context.Background(), &hs.HelloRequest{Name: n.Name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting from the other node: %s", r.Message)
+
+}
+
+// Busy Work module, greet every new member you find
+func (n *node) GreetAll() {
+	// get all nodes -- inefficient, but this is just an example
+	kvpairs, _, err := n.SDKV.List("Node", nil)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+
+	// fmt.Println("Found nodes: ")
+	for _, kventry := range kvpairs {
+		if strings.Compare(kventry.Key, n.Name) == 0 {
+			// ourself
+			continue
+		}
+		if n.Clients[kventry.Key] == nil {
+			fmt.Println("New member: ", kventry.Key)
+			// connection not established previously
+			n.SetupClient(kventry.Key, string(kventry.Value))
+		}
 	}
 }
 
@@ -111,20 +158,4 @@ func main() {
 
 	// start the node
 	noden.Start()
-
-	/*
-		// setup connection with other node
-		conn, err := grpc.Dial(address, grpc.WithInsecure())
-		if err != nil {
-			log.Fatalf("did not connect: %v", err)
-		}
-		defer conn.Close()
-		noden.C = hs.NewHelloServiceClient(conn)
-
-		r, err := noden.C.SayHello(context.Background(), &hs.HelloRequest{Name: "John"})
-		if err != nil {
-			log.Fatalf("could not greet: %v", err)
-		}
-		log.Printf("Greeting from the other node: %s", r.Message)
-	*/
 }
